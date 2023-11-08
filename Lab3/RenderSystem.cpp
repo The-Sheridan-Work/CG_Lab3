6cf99be612f08789594666f1589fe00d89a09fa4
sami-s-56 <85152176+sami-s-56@users.noreply.github.com>
2023-11-07 23:30:20 -0500

    Json File Read and Loaded

--- Lab3/RenderSystem.cpp
+++ Lab3/RenderSystem.cpp
@@ -1,9 +1,24 @@
 #include "RenderSystem.h"
 #include <iostream>
+#include "JsonHelper.h"
+
+RenderSystem* RenderSystem::instance = nullptr;
 
 void RenderSystem::Initialize()
 {
 	std::cout << "Render System Initialize()" << std::endl;
+
+	json::JSON document = JsonHelper::OpenFile("RenderSettings.h");
+
+	if (!document.IsNull())
+	{
+		name = JsonHelper::GetValue(document, "name").ToString();
+		width = JsonHelper::GetValue(document, "width").ToInt();
+		height = JsonHelper::GetValue(document, "hieght").ToInt();
+		bFullscreen = JsonHelper::GetValue(document, "fullscreen").ToBool();
+	}
+
+	//SDL Window Code
 }
 
 void RenderSystem::Load()
